d09e194b5920800334f3c722a8da8e060f5d5e20
Trincheira - Comunicação Visual <48634870+marvitphy@users.noreply.github.com>
2020-12-02 19:16:16 -0300

    resize nas fotos

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const bodyparser = require('body-parser')
 const path = require('path')
 const app = express()
+const sharp = require('sharp');
 
 
-var port = Number(process.env.PORT || 4000);
+var port = Number(process.env.PORT || 5000);
 
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
@@ -17,6 +18,14 @@ app.use(express.static('public'))
 app.get('/', function(req, res) {
     res.render('Dashboard')
 })
+
+
+
+sharp('./public/assets/3.jpg')
+    .resize(1312, 1968)
+    .toFile('3.webp', (err, info) => {});
+
+
 app.get('/api/addGig', function(req, res) {
     res.send('Error')
 })
